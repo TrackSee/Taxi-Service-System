16f4f01aba381b709d6766049d9577d898b21624
dvorig3 <idvorskij@mail.ru>
2015-04-17 19:10:06 +0300

    init mail notification
    add package com.tss.tracksee
    add pom dependency java.mail
    create empty class

---
+++ src/main/java/com/tss/tracksee/com/tss/tracksee/mailsender/MailSender.java
@@ -0,0 +1,12 @@
+package com.tss.tracksee.com.tss.tracksee.mailsender;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Igor
+ * Date: 17.04.15
+ * Time: 19:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class MailSender {
+
+}
