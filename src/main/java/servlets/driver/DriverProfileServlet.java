ef8f874f9f39a8d65777cfab829cb3041d53249a
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-04-18 03:59:16 +0300

    Servlets renamed with pattern *Servlet.java

--- src/main/java/servlets/driver/DriverProfileServlet.java
+++ src/main/java/servlets/driver/DriverProfileServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Created by Vadym Akymov on 15.04.15.
  */
 @WebServlet("/order/info")
-public class DriverProfile extends HttpServlet {
+public class DriverProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/driver/driverProfile.jsp").forward(req,resp);
