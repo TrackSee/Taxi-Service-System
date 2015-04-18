1ccc118d0c6e92e0bdc38e332c648a3bfcbe696b
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-04-18 16:45:39 +0300

    TaxiOrder improved

--- src/main/java/entity/TaxiOrder.java
+++ src/main/java/entity/TaxiOrder.java
@@ -11,5 +11,35 @@ public class TaxiOrder {
     private Timestamp creationTime;
     private Timestamp orderTime;
 
+    public Address getOrigin() {
+        return origin;
+    }
 
+    public void setOrigin(Address origin) {
+        this.origin = origin;
+    }
+
+    public Address getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Address destination) {
+        this.destination = destination;
+    }
+
+    public Timestamp getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Timestamp creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Timestamp getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Timestamp orderTime) {
+        this.orderTime = orderTime;
+    }
 }
