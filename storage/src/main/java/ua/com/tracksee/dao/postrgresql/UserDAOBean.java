facec6ac1a3add2f2dca19aa78abb25f630c6311
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-04-21 04:30:02 +0300

    UserDAO has new methods

--- storage/src/main/java/ua/com/tracksee/dao/postrgresql/UserDAOBean.java
+++ storage/src/main/java/ua/com/tracksee/dao/postrgresql/UserDAOBean.java
@@ -26,9 +26,7 @@ public class UserDAOBean implements UserDAO {
     private EntityManager entityManager;
 
 
-
     /**
-     * @author
      * @param partNumber - number of data part (from 1 to driver_count/DRIVERS_LIMIT)
      * @return list with part of drivers(default size of list if 10)
      */
@@ -43,5 +41,28 @@ public class UserDAOBean implements UserDAO {
         query.setParameter("limit", DRIVERS_LIMIT);
         query.setParameter("offset", (partNumber-1)*DRIVERS_LIMIT);
         return query.getResultList();
+    }
+
+    @Override
+    public void clearUnactivatedAccounts(int unactivatedDays) {
+        String sql = "DELETE FROM service_user " +
+                "WHERE activated = FALSE " +
+                "AND CURRENT_TIMESTAMP - registration_date  > '" + unactivatedDays + " days' :: INTERVAL";
+        Query query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+    }
+
+    @Override
+    public Boolean accountIsActivated(Integer userId) {
+        String sql = "SELECT  FROM service_user WHERE user_id = " + userId;
+        Query query = entityManager.createNativeQuery(sql);
+        return (Boolean) query.getSingleResult();
+    }
+
+    @Override
+    public void activateAccount(Integer userId) {
+        String sql = "";
+        Query query = entityManager.createNamedQuery(sql);
+        query.executeUpdate();
     }
 }
