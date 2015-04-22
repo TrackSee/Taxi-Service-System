98a8d31e12630139a9e6287b24a6c45116161895
Vadimka29 <vadimka2995@gmail.com>
2015-04-22 17:30:25 +0300

    admin page was included(with css, js and bower components)

--- presentation/src/main/java/ua/com/tracksee/servlets/admin/HomeAdminServlet.java
+++ presentation/src/main/java/ua/com/tracksee/servlets/admin/HomeAdminServlet.java
@@ -1,6 +1,5 @@
 package ua.com.tracksee.servlets.admin;
 
-import ua.com.tracksee.entities.ServiceUserEntity;
 import ua.com.tracksee.logic.admin.AdministratorBean;
 
 import javax.ejb.EJB;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by Vadym_Akymov on 21.04.15.
@@ -25,9 +23,6 @@ public class HomeAdminServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<ServiceUserEntity> list = administratorBean.getDrivers(1);
-        for(int i = 0; i  < list.size(); i ++) {
-            resp.getOutputStream().print(list.get(i).toString());
-        }
+        req.getRequestDispatcher("/WEB-INF/admin/admin.jsp").forward(req, resp);
     }
 }
