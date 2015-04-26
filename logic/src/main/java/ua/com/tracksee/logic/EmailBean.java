677b3904dfeaf617c79b1ca3999b80674c16cf93
dvorig3 <idvorskij@mail.ru>
2015-04-26 17:39:49 +0300

    mail notification
    1. Email invite to user when order changes state from “Queued” to “In
    progress” (EML3).
    2. Email invite to user when user included to blacklist (EML7).

--- logic/src/main/java/ua/com/tracksee/logic/EmailBean.java
+++ logic/src/main/java/ua/com/tracksee/logic/EmailBean.java
@@ -3,6 +3,7 @@ package ua.com.tracksee.logic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.com.tracksee.entities.ServiceUserEntity;
+import ua.com.tracksee.entities.TaxiOrderItemEntity;
 import ua.com.tracksee.util.EmailUtils;
 
 import javax.ejb.Asynchronous;
@@ -13,6 +14,10 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static ua.com.tracksee.mailsender.MailSender.*;
 
 /**
  * @author Ruslan Gunavardana
@@ -24,6 +29,15 @@ public class EmailBean {
     // website
     private static final String WEBSITE_SHORT = "tracksee.team.com/TaxiService";
     private static final String WEBSITE_FULL = "localhost:8080/TaxiService/";
+
+    // template properties
+    private static final String SITE_ADDRESS_TEMP_PROP_NAME = "siteadress";
+
+    private static final String BLOCKING_ACCOUNT_SUBJECT_TEMP_PROP_NAME = "TrackSee Blocking Account";
+    private static final String BLOCKING_ACCOUNT_TEMP_PATH = "logic/src/main/resources/mailtemplates/blockingusertemplate.ftl";
+
+    private static final String CHANGING_TO_FROM_ASSIGNED_TO_UPDATED_TEMP_PATH = "logic/src/main/resources/mailtemplates/changingtofromassignedtoupdatedtemplate.ftl";
+    private static final String CHANGING_TO_FROM_ASSIGNED_TO_UPDATED_SUBJECT_TEMP_PROP_NAME = "TrackSee Blocking Account";
 
     @Asynchronous
     public void sendRegistrationEmail(ServiceUserEntity user, String userCode) throws MessagingException {
@@ -44,10 +58,24 @@ public class EmailBean {
                 + WEBSITE_FULL + "activation?code=" + userCode;
     }
 
-//    @Asynchronous
-//    public void sendBlockingUserEmail(User user){
-//        String body = "Hi! Sorry, but our cooperation have become impossible from now. Have a nice day! ";
-//        String subject = "Blocking account";
-//        MailSender.sendEmail(user.getEmail(), subject, body);
-//    }
+    @Asynchronous
+    public void sendBlockingUserEmail(ua.com.tracksee.entities.ServiceUserEntity user) {
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(SITE_ADDRESS_TEMP_PROP_NAME, WEBSITE_FULL);
+        sendTemplatedEmail(user.getEmail(), BLOCKING_ACCOUNT_SUBJECT_TEMP_PROP_NAME, BLOCKING_ACCOUNT_TEMP_PATH, data);
+    }
+
+    @Asynchronous
+    public void sendChangingTOFromAssignedToCompleted(TaxiOrderItemEntity order) {
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(SITE_ADDRESS_TEMP_PROP_NAME, WEBSITE_FULL);
+        data.put("trackingNumber", order.getTaxiOrderByTrackingNumer().getTrackingNumber());
+        data.put("color", order.getServiceUserByDriverId().getCar().getColor());
+        data.put("carModel", order.getServiceUserByDriverId().getCar().getCarModel());
+        data.put("carNumber", order.getServiceUserByDriverId().getCar().getCarNumber());
+        sendTemplatedEmail(order.getServiceUserByDriverId().getEmail(),
+                CHANGING_TO_FROM_ASSIGNED_TO_UPDATED_SUBJECT_TEMP_PROP_NAME,
+                CHANGING_TO_FROM_ASSIGNED_TO_UPDATED_TEMP_PATH, data);
+
+    }
 }
