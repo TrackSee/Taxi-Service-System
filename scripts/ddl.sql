a8165294e1269897a565485a80d14e46a723a995
Vadimka29 <vadimka2995@gmail.com>
2015-04-27 14:44:16 +0300

    ddl.sql password varchar(28) was changed to password(150) P.S. because pass hash

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS Service_User
 (
   user_id           SERIAL PRIMARY KEY,
   email             VARCHAR(254) UNIQUE NOT NULL,
-  password          VARCHAR(28)         NOT NULL,
+  password          VARCHAR(150)         NOT NULL,
   phone             VARCHAR(28),
   sex               CHAR(1),
   driver            BOOLEAN             NOT NULL DEFAULT FALSE,
