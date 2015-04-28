41491036bd1e85476dbb9cd9ce28a29f20fe8fad
Oleksandr Kozin <kozin-o-y@ukr.net>
2015-04-28 14:55:30 +0300

    Add AddressEntityBean

---
+++ logic/src/main/java/ua/com/tracksee/logic/address/AddressEntityBean.java
@@ -0,0 +1,78 @@
+package ua.com.tracksee.logic.address;
+
+import ua.com.tracksee.dao.AddressDAO;
+import ua.com.tracksee.dao.UserDAO;
+import ua.com.tracksee.entities.AddressEntity;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.List;
+
+/**
+ * Created on 28.04.2015
+ *
+ * @author Oleksandr Kozin
+ */
+@Stateless
+public class AddressEntityBean {
+
+    @EJB
+    private UserDAO userDAO;
+    @EJB
+    private AddressDAO addressDAO;
+
+    public AddressEntityBean() {
+    }
+
+    /**
+     *
+     * @param userId -
+     * @param name -
+     * @return -
+     */
+    public AddressEntity getAddressByUserId(long userId, String name) {
+        return addressDAO.getAddressByUserId(userId, name);
+    }
+
+    /**
+     *
+     * @param userId -
+     * @return p-
+     */
+    public List getAllAddressesByUserId(long userId) {
+        return addressDAO.getAllAddressesByUserId(userId);
+    }
+
+    /**
+     *
+     * @param address number of part which is needed
+     * @return part of addresses
+     */
+    public List getAllAddressesByUserId(AddressEntity address){
+        return addressDAO.getAllAddressesByUserId(address);
+    }
+
+    /**
+     *
+     * @param address -
+     */
+    public void addAddress(AddressEntity address){
+        addressDAO.addAddress(address);
+    }
+
+    /**
+     *
+     * @param address -
+     */
+    public void deleteAddress(AddressEntity address){
+        addressDAO.deleteAddress(address);
+    }
+
+    /**
+     *
+     * @param address -
+     */
+    public void updateAddress(AddressEntity address){
+        addressDAO.updateAddress(address);
+    }
+}
