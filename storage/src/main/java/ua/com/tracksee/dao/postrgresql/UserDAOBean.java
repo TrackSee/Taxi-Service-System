f7dee60fb14827b9906b6fffa3f46193b7f8ad59
Vadym Akymov <vadimka2995@gmail.com>
2015-04-28 23:22:32 +0300

    drivers were sorted by email

--- storage/src/main/java/ua/com/tracksee/dao/postrgresql/UserDAOBean.java
+++ storage/src/main/java/ua/com/tracksee/dao/postrgresql/UserDAOBean.java
@@ -37,7 +37,7 @@ public class UserDAOBean implements UserDAO {
         throw new IllegalArgumentException("partNumber can't be <= 0");
     }
     Query query = entityManager.createNativeQuery("SELECT * FROM service_user " +
-            "WHERE driver = TRUE LIMIT ?1 OFFSET ?2", ServiceUserEntity.class);
+            "WHERE driver = TRUE ORDER BY email LIMIT ?1 OFFSET ?2", ServiceUserEntity.class);
     query.setParameter(1, DRIVERS_PAGE_SIZE);
     query.setParameter(2, (partNumber - 1) * DRIVERS_PAGE_SIZE);
     return query.getResultList();
