3ecb9d3a4d40831fe2019d89bce8fca701b67511
Oleksandr Kozin <kozin-o-y@ukr.net>
2015-04-30 12:42:04 +0300

    Rewrite AddressBean and CustomerBean

--- logic/src/main/java/ua/com/tracksee/logic/address/AddressEntityBean.java
+++ logic/src/main/java/ua/com/tracksee/logic/address/AddressEntityBean.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 /**
  * Created on 28.04.2015
- *
  * @author Oleksandr Kozin
  */
 @Stateless
--- logic/src/main/java/ua/com/tracksee/logic/customer/CustomerBean.java
+++ logic/src/main/java/ua/com/tracksee/logic/customer/CustomerBean.java
@@ -1,75 +1,13 @@
 package ua.com.tracksee.logic.customer;
 
-import ua.com.tracksee.dao.AddressDAO;
-import ua.com.tracksee.dao.UserDAO;
-import ua.com.tracksee.entities.AddressEntity;
-
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import java.util.List;
 
 /**
- * Created on 27.04.2015.
+ * Created on 27.04.2015
  * @author Oleksandr Kozin
  */
 @Stateless
 public class CustomerBean {
 
-    @EJB
-    private UserDAO userDAO;
-    @EJB
-    private AddressDAO addressDAO;
-
-    /**
-     *
-     * @param userId -
-     * @param name -
-     * @return -
-     */
-    public AddressEntity getAddressByUserId(Integer userId, String name) {
-        return addressDAO.getAddressByUserId(userId, name);
-    }
-
-    /**
-     *
-     * @param userId -
-     * @return p-
-     */
-    public List getAllAddressesByUserId(Integer userId) {
-        return addressDAO.getAllAddressesByUserId(userId);
-    }
-
-    /**
-     *
-     * @param address number of part which is needed
-     * @return part of addresses
-     */
-    public List getAllAddressesByUserId(AddressEntity address){
-        return addressDAO.getAllAddressesByUserId(address);
-    }
-
-    /**
-     *
-     * @param address -
-     */
-    public void addAddress(AddressEntity address){
-        addressDAO.addAddress(address);
-    }
-
-    /**
-     *
-     * @param address -
-     */
-    public void deleteAddress(AddressEntity address){
-        addressDAO.deleteAddress(address);
-    }
-
-    /**
-     *
-     * @param address -
-     */
-    public void updateAddress(AddressEntity address){
-        addressDAO.updateAddress(address);
-    }
 
 }
