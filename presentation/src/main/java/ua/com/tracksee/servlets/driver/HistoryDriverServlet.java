681dadb4951f1065e187ce96dae445a685c371a8
marykomar <maria.komar@gmail.com>
2015-05-04 23:49:47 +0300

    pagination

--- presentation/src/main/java/ua/com/tracksee/servlets/driver/HistoryDriverServlet.java
+++ presentation/src/main/java/ua/com/tracksee/servlets/driver/HistoryDriverServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Maria Komar on 20.04.2015.
@@ -32,7 +31,7 @@ public class HistoryDriverServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<TaxiOrderEntity> orders = driverOrderBean.getHistoryOfOrders(id, 1);
-        Map<Integer, String> addresses = driverOrderBean.getStringAddressForList(orders);
+        //Map<Integer, String> addresses = driverOrderBean.getStringAddressForList(orders);
         req.setAttribute("orders", orders);
         req.setAttribute("pagesCount", driverOrderBean.getOrdersPagesCount(id));
         //req.setAttribute("addresses", addresses);
@@ -42,10 +41,27 @@ public class HistoryDriverServlet extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<TaxiOrderEntity> orders = driverOrderBean.getHistoryOfOrders(id, 1);
+        Integer pageNumber = null;
+        //check pageNumber
+        try {
+            pageNumber = Integer.parseInt(req.getParameter("pageNumber"));
+            if(pageNumber > driverOrderBean.getOrdersPagesCount(id)){
+                pageNumber = 1;
+                logger.warn("wrong page was request");
+            }
+        } catch (NumberFormatException e){
+            pageNumber = 1;
+            logger.warn("wrong page was request");
+        }
+        List<TaxiOrderEntity> orders = driverOrderBean.getHistoryOfOrders(id, pageNumber);
         req.setAttribute("orders", orders);
         req.setAttribute("pagesCount", driverOrderBean.getOrdersPagesCount(id));
-        req.getRequestDispatcher("/WEB-INF/driver/historyDriverTo.jsp").forward(req,resp);
+        resp.getWriter().write(getJsonFromList(orders));
+
+//        List<TaxiOrderEntity> orders = driverOrderBean.getHistoryOfOrders(id, 1);
+//        req.setAttribute("orders", orders);
+//        req.setAttribute("pagesCount", driverOrderBean.getOrdersPagesCount(id));
+//        req.getRequestDispatcher("/WEB-INF/driver/historyDriverTo.jsp").forward(req,resp);
     }
 
     private String getJsonFromList(List<TaxiOrderEntity> orders){
--- storage/src/main/java/ua/com/tracksee/dao/TaxiOrderDAO.java
+++ storage/src/main/java/ua/com/tracksee/dao/TaxiOrderDAO.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 @Local
 public interface TaxiOrderDAO {
-    int ORDERS_PAGE_SIZE = 10;
+    int ORDERS_PAGE_SIZE = 2;
 
     public void addComment(TaxiOrderEntity taxiOrderEntity);
 
