2ece3c1fb3701276a02abcdb8a512eae4366d740
Oleksandr Kozin <kozin-o-y@ukr.net>
2015-05-04 15:55:38 +0300

    add methods add documentation to TaxiPriceDAO which make a report of service profitability by month

--- storage/src/main/java/ua/com/tracksee/dao/TaxiPriceDAO.java
+++ storage/src/main/java/ua/com/tracksee/dao/TaxiPriceDAO.java
@@ -27,4 +27,78 @@ public interface TaxiPriceDAO {
      * @param priceEntity
      */
     void updateTariff(TaxiPriceEntity priceEntity);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Simple Taxi" service
+     */
+    int profitByMonthSimpleTaxi(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Sober Driver" service
+     */
+    int profitByMonthSoberDriver(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the Convey "Corporation Employees" service
+     */
+    int profitByMonthConveyCorpEmp(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Guest Delivery" service
+     */
+    int profitByMonthGuestDelivery(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Cargo Taxi" service
+     */
+    int profitByMonthCargoTaxi(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Taxi For Long Term" service
+     */
+    int profitByMonthTaxiForLongTerm(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Meet My Guest" service
+     */
+    int profitByMonthMeetMuGuest(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Celebration Taxi" service
+     */
+    int profitByMonthCelebrationTaxi(String year, String month);
+
+    /**
+     * @author Oleksandr Kozin
+     * @param year year of report
+     * @param month month of report
+     * @return sum of prices for all the orders for the "Foodstuff Delivery" service
+     */
+    int profitByMonthFoodstuffDelivery(String year, String month);
+
+
 }
