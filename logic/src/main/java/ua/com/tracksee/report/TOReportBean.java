d622e2584abbcdecb66b8b70b267e411311dd2aa
Oleksandr Kozin <kozin-o-y@ukr.net>
2015-05-05 18:37:33 +0300

    rewrite TOReportBean

--- logic/src/main/java/ua/com/tracksee/report/TOReportBean.java
+++ logic/src/main/java/ua/com/tracksee/report/TOReportBean.java
@@ -1,9 +1,12 @@
 package ua.com.tracksee.report;
 
 import ua.com.tracksee.dao.TaxiOrderDAO;
+import ua.com.tracksee.dao.TaxiPriceDAO;
+import ua.com.tracksee.entities.ProfitEntity;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
  * Created on 02.05.2015
@@ -13,11 +16,10 @@ import javax.ejb.Stateless;
 @Stateless
 public class TOReportBean {
 
-//    New orders per period
-//    Service profitability by month
-
     @EJB
-    TaxiOrderDAO taxiOrderDAO;
+    private TaxiOrderDAO taxiOrderDAO;
+    @EJB
+    private TaxiPriceDAO taxiPriceDAO;
 
     /**
      * @param startDate start date of the period
@@ -28,5 +30,12 @@ public class TOReportBean {
         return taxiOrderDAO.getOrdersByPeriod(startDate, endDate);
     }
 
-
+    /**
+     * @param year year of report
+     * @param month month of report
+     * @return list with sum of prices grouped by service for all orders
+     */
+    public List<ProfitEntity> serviceProfitByMonth(String year, String month) {
+        return taxiPriceDAO.serviceProfitByMonth(year, month);
+    }
 }
