2840f186999726f568f5e6f7ab8378d40929e603
Oleksandr Kozin <kozin-o-y@ukr.net>
2015-05-05 18:36:56 +0300

    rewrite method add documentation to TaxiPriceDAO which make a report of service profitability by month add ProfitEntity

--- storage/src/main/java/ua/com/tracksee/dao/TaxiPriceDAO.java
+++ storage/src/main/java/ua/com/tracksee/dao/TaxiPriceDAO.java
@@ -1,9 +1,11 @@
 package ua.com.tracksee.dao;
 
+import ua.com.tracksee.entities.ProfitEntity;
 import ua.com.tracksee.entities.TaxiPriceEntity;
 import ua.com.tracksee.enumartion.CarCategory;
 
 import javax.ejb.Local;
+import java.util.List;
 
 /**
  * @author Ruslan Gunavardana
@@ -32,73 +34,7 @@ public interface TaxiPriceDAO {
      * @author Oleksandr Kozin
      * @param year year of report
      * @param month month of report
-     * @return sum of prices for all the orders for the "Simple Taxi" service
+     * @return list with sum of prices grouped by service for all orders
      */
-    int profitByMonthSimpleTaxi(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the "Sober Driver" service
-     */
-    int profitByMonthSoberDriver(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the Convey "Corporation Employees" service
-     */
-    int profitByMonthConveyCorpEmp(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the "Guest Delivery" service
-     */
-    int profitByMonthGuestDelivery(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the "Cargo Taxi" service
-     */
-    int profitByMonthCargoTaxi(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the "Taxi For Long Term" service
-     */
-    int profitByMonthTaxiForLongTerm(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the "Meet My Guest" service
-     */
-    int profitByMonthMeetMuGuest(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the "Celebration Taxi" service
-     */
-    int profitByMonthCelebrationTaxi(String year, String month);
-
-    /**
-     * @author Oleksandr Kozin
-     * @param year year of report
-     * @param month month of report
-     * @return sum of prices for all the orders for the "Foodstuff Delivery" service
-     */
-    int profitByMonthFoodstuffDelivery(String year, String month);
-
-
+    List<ProfitEntity> serviceProfitByMonth(String year, String month);
 }
--- storage/src/main/java/ua/com/tracksee/dao/postrgresql/TaxiPriceDAOBean.java
+++ storage/src/main/java/ua/com/tracksee/dao/postrgresql/TaxiPriceDAOBean.java
@@ -1,6 +1,7 @@
 package ua.com.tracksee.dao.postrgresql;
 
 import ua.com.tracksee.dao.TaxiPriceDAO;
+import ua.com.tracksee.entities.ProfitEntity;
 import ua.com.tracksee.entities.TaxiPriceEntity;
 import ua.com.tracksee.enumartion.CarCategory;
 
@@ -8,6 +9,9 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Ruslan Gunavardana
@@ -31,5 +35,26 @@ public class TaxiPriceDAOBean implements TaxiPriceDAO {
     @Override
     public void updateTariff(TaxiPriceEntity priceEntity) {
         entityManager.refresh(priceEntity);
+    }
+
+    @Override
+    public List<ProfitEntity> serviceProfitByMonth(String year, String month){
+        String yearMonth = year + "-" + month;
+        String sql = "SELECT service, SUM(price) FROM taxi_order" +
+                " WHERE ordered_date" +
+                " BETWEEN '" + yearMonth + "-01'" +
+                " AND '" + yearMonth + "-30'" +
+                " GROUP BY service";
+        Query query = entityManager.createNativeQuery(sql);
+        List<Object[]> list = query.getResultList();
+        List<ProfitEntity> profitList = new ArrayList<>();
+        ProfitEntity pe;
+        for (Object[] objects : list) {
+            String s = (String) objects[0];
+            BigDecimal b = (BigDecimal) objects[1];
+            pe = new ProfitEntity(s, b.doubleValue());
+            profitList.add(pe);
+        }
+        return profitList;
     }
 }
