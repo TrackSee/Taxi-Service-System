791a4a7a30c5341489f0932a7242873c088cc282
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-05-09 00:57:08 +0300

    Bo added

---
+++ logic/src/main/java/ua/com/tracksee/logic/bo/CustomerUser.java
@@ -0,0 +1,16 @@
+package ua.com.tracksee.logic.bo;
+
+import javax.enterprise.context.RequestScoped;
+
+/**
+ * @author Ruslan Gunavardana
+ */
+@RequestScoped
+public class CustomerUser {
+    private int userId;
+    private String email;
+    private String phone;
+
+
+
+}
---
+++ logic/src/main/java/ua/com/tracksee/logic/bo/Driver.java
@@ -0,0 +1,10 @@
+package ua.com.tracksee.logic.bo;
+
+import javax.enterprise.context.RequestScoped;
+
+/**
+ * @author Ruslan Gunavardana
+ */
+@RequestScoped
+public class Driver {
+}
