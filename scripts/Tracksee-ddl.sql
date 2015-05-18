164cdf1e764687866724555167e32fa980e6cd02
vataga <diravka_vetal@mail.ru>
2015-05-18 13:51:41 +0300

    revert this (added field NAME  to the table service_user)

--- scripts/Tracksee-ddl.sql
+++ scripts/Tracksee-ddl.sql
@@ -42,7 +42,6 @@ CREATE TABLE IF NOT EXISTS tracksee.public.Service_User
   email             VARCHAR(254) UNIQUE NOT NULL,
   password          CHAR(64)            NOT NULL,
   salt              CHAR(8)             NOT NULL,
-  name              CHAR(20)            NOT NULL,
   phone             VARCHAR(28),
   first_name        VARCHAR(50),
   last_name         VARCHAR(50),
