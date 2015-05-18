987fc4ef7d574831c6b28240f9e83ad2616b30b7
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-05-18 22:27:03 +0300

    some small fixes

--- presentation/src/main/webapp/resources/customer/js/sign-in.js
+++ presentation/src/main/webapp/resources/customer/js/sign-in.js
@@ -1,21 +1,23 @@
 /**
  * Created by Ruslan Gunavardana
  */
-function sendForm() {
-    $.ajax({
-        type: 'POST',
-        url: getContextPath() + 'signin',
-        data: $('#form-sign-in').serialize(),
-        success: function () {
-            window.location.replace('.');
-        },
-        error: function (xhr) {
-            var btn = $('#sign-in-submit');
-            if (xhr.status == 422) {
-                btn.notify("The username or password is incorrect. Please try again.", "error");
-            } else {
-                btn.notify("Internal server error occurred.", "warn");
+$(document).ready(function(){
+    $('#form-sign-in').submit(function() {
+        $.ajax({
+            type: 'POST',
+            url: getContextPath() + 'signin',
+            data: $(this).serialize(),
+            success: function () {
+                window.location.replace('.');
+            },
+            error: function (xhr) {
+                var btn = $('#sign-in-submit');
+                if (xhr.status == 422) {
+                    btn.notify("The username or password is incorrect. Please try again.", "error");
+                } else {
+                    btn.notify("Internal server error occurred.", "warn");
+                }
             }
-        }
+        });
     });
-}
+});
