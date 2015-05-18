24256ae181dd005a630f505188657d129b0afdd5
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-05-18 22:27:03 +0300

    some small fixes

--- presentation/src/main/webapp/resources/customer/js/sign-out.js
+++ presentation/src/main/webapp/resources/customer/js/sign-out.js
@@ -1,39 +1,41 @@
 /**
  * Created by Ruslan Gunavardana
  */
-function signOut() {
-    $.ajax({
-        type: 'POST',
-        url: getContextPath() + 'signout',
-        success: function(){
-            updateHeader();
-        },
-        error: function(){
-            updateHeader();
-        }
+document.addEventListener("DOMContentLoaded", function(event) {
+    $('#sign-out-button').click(function(){
+        $.ajax({
+            type: 'POST',
+            url: getContextPath() + 'signout',
+            success: function () {
+                updateHeader();
+            },
+            error: function () {
+                updateHeader();
+            }
+        });
     });
-}
 
-function updateHeader() {
-    if (window.location.href.search('customerProfile') != -1) {
-        window.location.href = getContextPath();
-    } else {
-        $('#signout').remove();
-        $('#customerProfile').remove();
-        var ul = $('#button-list');
-        var elems = [
-            {text: 'Sign in', href: 'signin'},
-            {text: 'Sign up', href: 'signup'}
-        ];
-        var liArr = [];
-        var aArr = [];
-        for (var i = 0; i < elems.length; i++) {
-            liArr[i] = document.createElement('li');
-            aArr[i] = document.createElement('a');
-            aArr[i].href = getContextPath() + elems[i].href;
-            aArr[i].appendChild(document.createTextNode(elems[i].text));
-            liArr[i].appendChild(aArr[i]);
-            ul.append(liArr[i]);
+    function updateHeader() {
+        if (window.location.href.search('customerProfile') != -1) {
+            window.location.href = getContextPath();
+        } else {
+            $('#signout').remove();
+            $('#customerProfile').remove();
+            var ul = $('#button-list');
+            var elems = [
+                {text: 'Sign in', href: 'signin'},
+                {text: 'Sign up', href: 'signup'}
+            ];
+            var liArr = [];
+            var aArr = [];
+            for (var i = 0; i < elems.length; i++) {
+                liArr[i] = document.createElement('li');
+                aArr[i] = document.createElement('a');
+                aArr[i].href = getContextPath() + elems[i].href;
+                aArr[i].appendChild(document.createTextNode(elems[i].text));
+                liArr[i].appendChild(aArr[i]);
+                ul.append(liArr[i]);
+            }
         }
     }
-}
+});
