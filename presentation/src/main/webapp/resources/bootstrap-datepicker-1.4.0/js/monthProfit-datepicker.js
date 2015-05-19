9324c05d5c60941a1d0e431d9a6dd7fa679809f8
Oleksandr Kozin <kozin-o-y@ukr.net>
2015-05-19 23:28:11 +0300

    fix bug with date format

--- presentation/src/main/webapp/resources/bootstrap-datepicker-1.4.0/js/monthProfit-datepicker.js
+++ presentation/src/main/webapp/resources/bootstrap-datepicker-1.4.0/js/monthProfit-datepicker.js
@@ -2,7 +2,7 @@
  * Created by Oleksandr Kozin on 17.05.2015.
  */
 $('.input-group.date').datepicker({
-    format: "MM-yyyy",
+    format: "yyyy-MM",
     startView: 1,
     minViewMode: 1,
     autoclose: true,
