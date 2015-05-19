d25553656403c36de40bc7f9a57296898de81ddd
Oleksandr Kozin <kozin-o-y@ukr.net>
2015-05-20 02:55:14 +0300

    add json of most popular driver and car category to js

--- presentation/src/main/webapp/resources/admin/reports/js/popularCar.js
+++ presentation/src/main/webapp/resources/admin/reports/js/popularCar.js
@@ -2,37 +2,40 @@
  * Created by Олександр on 16.05.2015.
  */
 
-var popCarChart = AmCharts.makeChart( "popularCarDiv", {
-    "type": "pie",
-    "theme": "light",
-    "path": "http://www.amcharts.com/lib/3/",
-    "dataProvider": [ {
-        "car category": "Business class",
-        "ordersCount": 4852
-    }, {
-        "car category": "Economy class",
-        "ordersCount": 5555
-    }, {
-        "car category": "Van",
-        "ordersCount": 9899
-    } ],
-    "titleField": "car category",
-    "valueField": "ordersCount",
-    "labelRadius": 5,
-
-    "radius": "42%",
-    "innerRadius": "60%",
-    "labelText": "[[title]]",
-    "export": {
-        "enabled": true,
-        "libs": {
-            "path": "http://www.amcharts.com/lib/3/plugins/export/libs/"
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'car-category',
+        success: function (data) {
+            console.log(data);
+            var popDriverChart = AmCharts.makeChart("popularCarDiv", {
+                "dataProvider": JSON.parse(data),
+                "titleField": "car_category",
+                "valueField": "ordered_count",
+                "type": "pie",
+                "theme": "light",
+                "path": "http://www.amcharts.com/lib/3/",
+                "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+                "radius": "42%",
+                "innerRadius": "60%",
+                "allLabels": [],
+                "balloon": {},
+                "titles": [],
+                "labelText": "[[title]]",
+                "export": {
+                    "enabled": true,
+                    "libs": {
+                        "path": "http://www.amcharts.com/lib/3/plugins/export/libs/"
+                    }
+                    ,
+                    "menu": [{
+                        "format": "XLSX",
+                        "label": "Save as XLSX",
+                        "title": "Export chart to XLSX"
+                    }]
+                }
+            });
+            $('.amcharts-chart-div a').remove();
         }
-        ,
-        "menu": [{
-            "format": "XLSX",
-            "label": "Save as XLSX",
-            "title": "Export chart to XLSX"
-        }]
-    }
-} );
+    });
+});
