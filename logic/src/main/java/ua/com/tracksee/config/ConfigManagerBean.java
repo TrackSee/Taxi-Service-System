f7fb06fcefe0f44b362522291301ff6ad1ed23b7
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-05-24 08:17:56 +0300

    Price calculation includes animal transportation

--- logic/src/main/java/ua/com/tracksee/config/ConfigManagerBean.java
+++ logic/src/main/java/ua/com/tracksee/config/ConfigManagerBean.java
@@ -7,8 +7,12 @@ import ua.com.tracksee.exception.ConfigException;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import static java.lang.Double.parseDouble;
 import static ua.com.tracksee.config.ConfigName.MINIMAL_ORDER_DISTANCE;
+import static ua.com.tracksee.config.ConfigName.ANIMAL_TRANSPORTATION_MULTIPLIER;
 
 /**
  * Used to guarantee fast access to configs
@@ -29,5 +33,14 @@ public class ConfigManagerBean {
 
     public void setMinimalOrderDistance(double minimalOrderDistance) {
         configDao.saveConfig(new ConfigEntity(MINIMAL_ORDER_DISTANCE, Double.toString(minimalOrderDistance)));
+    }
+
+    public BigDecimal getAnimalTransportationMultiplier() {
+        String string = configDao.getConfig(ANIMAL_TRANSPORTATION_MULTIPLIER).getValue();
+        try {
+            return new BigDecimal(string);
+        } catch (NumberFormatException | NullPointerException e) {
+            throw new ConfigException("MINIMAL_ORDER_DISTANCE configured in the wrong way");
+        }
     }
 }
