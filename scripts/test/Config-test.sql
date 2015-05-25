737cb8ff81d1f12f7f52822ac7aa479b7903d4ec
Ruslan Gunavardana <rusan.rus@gmail.com>
2015-05-25 21:33:14 +0300

    New configs added to script

--- scripts/test/Config-test.sql
+++ scripts/test/Config-test.sql
@@ -1,3 +1,4 @@
 DELETE FROM tracksee.public.Config;
 
 INSERT INTO public.Config (name, value) VALUES ('minimal_order_distance', '5');
+INSERT INTO public.Config (name, value) VALUES ('animal_transportation_multiplier', '1.1');
